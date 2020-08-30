35f3bc0519cf7e0ac88687232fc56d0c97ad6481
varekaiwork <serhiisaviukwork@gmail.com>
2020-08-30 15:46:41 +0300

    start project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Text from './Text'
 import ToDoInput from './ToDoInput'
+import ToDoItem from './ToDoItem'
 
 ReactDOM.render(
   <div className="application">
     <Text size={18}>Todos </Text>
     <ToDoInput onAdd={(todo) => console.log(todo)} />
+    <ToDoItem
+      onSwitch={(todo) => console.log(todo)}
+      todo={{
+        _id: 'test',
+        text: 'React',
+        completed: false
+      }}
+    />
   </div>,
   document.getElementById('root')
 )
