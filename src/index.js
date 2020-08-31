17ef3ce56fb51917bc921579f4facd1b55f4a5f4
varekaiwork <serhiisaviukwork@gmail.com>
2020-08-31 17:02:33 +0300

    add localstorage, delete item

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
-import Text from './Text'
-import ToDoInput from './ToDoInput'
-import ToDoItem from './ToDoItem'
 
-ReactDOM.render(
-  <div className="application">
-    <Text size={18}>Todos </Text>
-    <ToDoInput onAdd={(todo) => console.log(todo)} />
-    <ToDoItem
-      onSwitch={(todo) => console.log(todo)}
-      todo={{
-        _id: 'test',
-        text: 'React',
-        completed: false
-      }}
-    />
-  </div>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
